5faa93a29d7852c2dbecd10580687009524759d5
krislee1204 <kcl1204@uw.edu>
2020-02-06 21:58:04 -0800

    Upload Week 5 Exercises

--- chapter-11-exercises/exercise-6/exercise.R
+++ chapter-11-exercises/exercise-6/exercise.R
@@ -1,4 +1,4 @@
-# Exercise 6: dplyr join operations
+# Exercise 6: dplyr join operations  
 
 # Install the `"nycflights13"` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
@@ -10,9 +10,19 @@ library("dplyr")
 # use `left_join()` to join on the "airports" dataframe, which has the airport
 # information
 # Which airport had the largest average arrival delay?
+avg_delay_df <- flights %>%
+  group_by(dest) %>%
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  rename(faa = dest)
 
+left_join(avg_delay_df, airports, by = "faa") %>%
+  select(faa, avg_arr_delay, name)
 
 # Create a dataframe of the average arrival delay for each _airline_, then use
 # `left_join()` to join on the "airlines" dataframe
 # Which airline had the smallest average arrival delay?
-
+flights %>%
+  group_by(carrier) %>%
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE)) %>%
+  left_join(airlines, by = "carrier") %>%
+  arrange(-avg_arr_delay)
