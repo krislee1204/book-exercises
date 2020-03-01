d98e797a54b651f31347272b46bea024188fc369
krislee1204 <59711265+krislee1204@users.noreply.github.com>
2020-03-01 13:06:01 -0800

    Update app_ui.R

--- chapter-19-exercises/exercise-4/app_ui.R
+++ chapter-19-exercises/exercise-4/app_ui.R
@@ -7,27 +7,31 @@ library("shiny")
 # Define a variable `intro_panel` for your first page. It should be a
 # `tabPanel()` with a title "Introduction" to represent the first tab.
 # This layout will contain the following elements:
-  
+  intro_panel <- tabPanel(
+    title = "Introduction"
+
   # A `titlePanel()` with the text "Income Inequality"
-  
+  titlePanel("Income Inequality"),
 
   # A paragraph `p()` describing with the text: "The below diagram was created
   # by the New York Times to illustrate the increasing level of inequality in
   # the US."
-  
+  p(inputID = "The below diagram was created by the New York Times to illustrate the increasing level of inequality in the US.")
 
   # An image `img()` to display. This content should have a `src` attribute of
   # "inequality.png" (which will refer to the file in the `www/` folder).
-  
+  img(src = "inequality.png", alt = "Example NYT chart")
 
   # A paragraph containing a hyperlink `a()` to the source of the article at
   # https://www.nytimes.com/interactive/2017/08/07/opinion/leonhardt-income-inequality.html
-
+  p(
+    a(href = "https://www.nytimes.com/interactive/2017/08/07/opinion/leonhardt-income-inequality.html", "(source)")
+  )
 
   # A paragraph containing a 1-2 sentence description of what the diagram
   # shows. Your paragraph should include both `strong()` and emphasized `em()`
   # text.
-  
+  )
 
 # Next, we'll define a few UI elements to render in our second panel
 
@@ -68,4 +72,6 @@ library("shiny")
 # The layout should include the following elements:
 # - Your `intro_panel`
 # - Your `growth_panel`
-
+ui <- navbarPage(
+  title = "Income Ineqality"
+)
